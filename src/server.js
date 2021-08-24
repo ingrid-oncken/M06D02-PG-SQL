4dec3b07406910fa296bf2c66eb54796ae4d4466
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-08-24 12:44:58 +0100

    setting up enviroment

--- src/server.js
+++ src/server.js
@@ -1,20 +1,22 @@
-import express from "express";
-import cors from "cors";
-import services from "./services/index.js";
-import createDefaultTables from "./scripts/create-tables.js";
+import express from "express"
+import cors from "cors"
+import services from "./services/index.js"
+import createDefaultTables from "./scripts/create-tables.js"
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-app.use(cors());
+app.use(cors())
 
-const { PORT } = process.env;
+const { PORT } = process.env
 
-app.use("/", services);
+app.use("/", services)
 
 app.listen(PORT, async () => {
-	await createDefaultTables();
-	console.log(`✅ Server is running on port  ${PORT}`);
-});
-app.on("error", (error) => console.log(`❌ Server is failed  :  ${error}`));
+  await createDefaultTables()
+  console.log(`✅ Server is running on port  ${PORT}`)
+})
+app.on("error", (error) =>
+  console.log(`❌ The server is not running, error:  ${error}`)
+)
